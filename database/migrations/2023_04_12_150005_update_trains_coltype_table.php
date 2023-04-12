79e9f1dd5985ae258ea3f577dcb1d7de0555d269
Blaser993 <corradodepinto.design@gmail.com>
2023-04-12 17:11:04 +0200

    FEAT cambio tipo colonne

---
+++ database/migrations/2023_04_12_150005_update_trains_coltype_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->time('orario_partenza')->change();
+            $table->time('orario_arrivo')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('orario_partenza')->change();
+            $table->string('orario_arrivo')->change();
+        });
+    }
+};
